bfe7fc96e85a3f59601939eee729228072a05f09
Quqin917 <irwan.purwo.a@gmail.com>
2024-11-04 13:19:42 +0700

    From new linux-main branch

--- src/input_output.cpp
+++ src/input_output.cpp
@@ -11,33 +11,7 @@ std::string getUserInput( const std::string& prompt ) {
   return input;
 }
 
-#ifdef _WIN32
 
 // Function to get password input from windows, hiding characters as they're typed
 std::string getPassword( const std::string& prompt ) {
-  DWORD mode = 0;
-  HANDLE inputHandle = GetStdHandle( STD_INPUT_HANDLE );
-
-  // Save current console mode
-  GetConsoleMode( inputHandle, &mode );
-
-  // Disable echoing of input 
-  SetConsoleMode( inputHandle, mode & (~ENABLE_ECHO_INPUT));
-
-  // Get the password input
-  std::string input { getUserInput(prompt) };
-
-  // Restore console mode to previous state
-  SetConsoleMode( inputHandle, mode );
-  
-  return input;
 }
-
-#else if __linux__
-
-// Function to ge password input from linux, hiding characters as they're typed
-std::string getPassword( const std::string& prompt ) {
-  
-}
-
-#endif
